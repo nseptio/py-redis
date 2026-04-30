50ede06b78e413059b99be6a83f8599b4c30800a
Septio Nugroho <septio.nugroho@ui.ac.id>
2026-04-30 09:27:40 +0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import asyncio
 from enum import Enum
-from typing import Optional, List
+from typing import List, Optional
+
+DATA = {}
 
 
 class RESPKind(Enum):
@@ -13,22 +15,22 @@ class RESPKind(Enum):
 
 async def parse_resp(reader: asyncio.StreamReader) -> Optional[List[bytes]]:
     try:
-        line = await reader.readuntil(b"\r\n")  # *2 \r\n
+        line = await reader.readuntil(b"\r\n")
         if not line:
             return None
 
-        kind = RESPKind(line[0:1].decode())  # *
+        kind = RESPKind(line[0:1].decode())
         if kind == RESPKind.ARRAY:
-            num_elements = int(line[1:-2])  # 2
+            num_elements = int(line[1:-2])
             command_parts: List[bytes] = []
 
             for _ in range(num_elements):
-                bulk_line = await reader.readuntil(b"\r\n")  # $4
+                bulk_line = await reader.readuntil(b"\r\n")
                 bulk_kind = RESPKind(bulk_line[0:1].decode())
                 if bulk_kind != RESPKind.BULK_STRING:
                     raise ValueError("Expected bulk string in array")
 
-                length = int(bulk_line[1:-2])  # 4
+                length = int(bulk_line[1:-2])
                 string_data = await reader.readexactly(
                     length + 2
                 )  # consume the trailing \r\n
@@ -56,8 +58,28 @@ async def handle_client(
             writer.write(b"+PONG\r\n")
         elif command == b"ECHO":
             argument = command_parts[1]
-            kind = RESPKind.BULK_STRING.value.encode()
-            response = kind + f"{len((argument))}\r\n".encode() + argument + b"\r\n"
+            bulk_string_prefix = RESPKind.BULK_STRING.value.encode()
+            response = (
+                bulk_string_prefix
+                + f"{len(argument)}\r\n".encode()
+                + argument
+                + b"\r\n"
+            )
+            writer.write(response)
+        elif command == b"SET":
+            key, value = command_parts[1], command_parts[2]
+            DATA[key] = value
+            writer.write(b"+OK\r\n")
+        elif command == b"GET":
+            key = command_parts[1]
+            value = DATA.get(key)
+            bulk_string_prefix = RESPKind.BULK_STRING.value.encode()
+            if value is None:
+                writer.write(bulk_string_prefix + b"-1\r\n")
+                return
+            response = (
+                bulk_string_prefix + f"{len(value)}\r\n".encode() + value + b"\r\n"
+            )
             writer.write(response)
         else:
             writer.write(b"-ERR unknown command\r\n")
@@ -67,7 +89,7 @@ async def handle_client(
     await writer.wait_closed()
 
 
-async def main():
+async def main() -> None:
     print("Starting Redis-like server on localhost:6379...")
     server = await asyncio.start_server(handle_client, "localhost", 6379)
 
